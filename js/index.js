c8293d3a272c11fe562d006edd9a6749a89d4853
ShawnCockburn <shawncockburn@hotmail.com>
2021-02-22 18:12:27 +0000

    Added tiltjs for cards only on desktop

--- js/index.js
+++ js/index.js
@@ -3,8 +3,26 @@ const nav = document.querySelector("nav");
 const mobileBtnExit = document.getElementById("mobile-exit");
 
 mobileBtn.addEventListener("click", () => {
-    nav.classList.add("menu-btn");
+  nav.classList.add("menu-btn");
 });
 mobileBtnExit.addEventListener("click", () => {
-    nav.classList.remove("menu-btn");
+  nav.classList.remove("menu-btn");
 });
+
+const setTilt = () => {
+  //if size is desktop enable tilt
+  if (window.innerWidth > 1250) {
+    $(".tilt-outer").tilt({
+      maxTilt: 5,
+      perspective: 300,
+      easing: "cubic-bezier(.03,.98,.52,.99)",
+      scale: 1.2,
+    });
+  } else {
+    const elements = $(".tilt-outer");
+    elements.tilt.destroy.call(elements);
+  }
+};
+
+$(document).ready(() => setTilt());
+window.addEventListener("resize", setTilt);
